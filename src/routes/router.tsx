cd7673df45d0739932062ed2649ae58928183b2b
arafatYasir <98328335+arafatYasir@users.noreply.github.com>
2025-03-06 15:28:58 +0600

    React router setup

---
+++ src/routes/router.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <h2>HELLO WORLD</h2>
+    }
+])
+
+export default router;
